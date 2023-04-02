653c5fadc31d744de10feeef4df393239f36f41a
Adrian Hernandez Mendez <aahdezm@gmail.com>
2023-04-02 19:05:02 +0200

    Improve getRouteData

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,31 @@
 import glob from 'glob';
+import { getBaseUrl, getRelativePath } from './utils/path.utils';
 
 interface RouteData {
   path: string;
   file: string;
 }
 
-export function getRouteData(pattern: string): RouteData[] {
-  const files = glob.sync(pattern);
+export function getRouteData(importBasePath: string, pagesPattern: string): RouteData[] {
+  const filePaths = glob.sync(pagesPattern);
+  const baseUrl = getBaseUrl(filePaths);
+  const relativePath = getRelativePath(importBasePath, baseUrl);
+  const nestedRoute = baseUrl.replace(/.*\/pages(\/)?/, '');
 
-  return files.map((file) => {
-    const path = file
-      .replace(/^src\/app\/pages\/index\.component\.ts$/, '') // handle special case of index.component.ts
-      .replace(/^src\/app\/pages\//, '') // remove src/pages and first slash
+  return filePaths.map((filePath) => {
+    const pathSegment = filePath
+      .replace(new RegExp(`^${baseUrl}/index.component.ts$`), '') // handle special case of index.component.ts
+      .replace(new RegExp(`^${baseUrl}/`), '') // remove baseUrl and first slash
       .replace(/(?:\/index)?(?:\.component)?\.ts$/, '') // remove suffixes
       .replace(/\[\.{3}.+\]/, '*') // replace [...foo] with *
       .replace(/\[(.+)\]/, ':$1') // replace [foo] with :foo
       .replace(/^(.+?)\/$/, '$1'); // remove trailing slash
 
-    return {
-      path,
-      file: file.replace(/\.ts$/, '').replace(/^src\/app/, '.')
-    };
+    const path = nestedRoute !== '' ? `${nestedRoute}/${pathSegment}` : pathSegment;
+
+    const relativePathPrefix = relativePath.startsWith('..') ? '' : './';
+    const file = filePath.replace(/\.ts$/, '').replace(new RegExp(`^${baseUrl}`), `${relativePathPrefix}${relativePath}`);
+
+    return { path, file };
   });
 }
